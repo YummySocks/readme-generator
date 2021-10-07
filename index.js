adefe20923534bfd9db88603624d6921795944b6
Socks <patricksutcliffe123@gmail.com>
2021-10-06 21:44:04 -0400

    started the init function

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
+// TODO: Include packages needed for this application
+
 const inquirer = require("inquirer");
 
-// TODO: Include packages needed for this application
-inquirer
 // TODO: Create an array of questions for user input
 const questions = [
     {
@@ -55,7 +55,9 @@ const questions = [
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+}
 
 // Function call to initialize app
 init();
