7ab35d3ed72907d50d80b7f4be0345713f8689de
Socks <patricksutcliffe123@gmail.com>
2021-10-06 21:34:53 -0400

    added all the questions needed

--- index.js
+++ index.js
@@ -1,7 +1,55 @@
-// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
 
+// TODO: Include packages needed for this application
+inquirer
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is your GitHub username?',
+        name: 'userName'
+    },
+    {
+        type: 'input',
+        message: 'What is your email address?',
+        name: 'email'
+    },
+    {
+        type: 'input',
+        message: "What is your project's name?",
+        name: 'project'
+    },
+    {
+        type: 'input',
+        message: 'Please write a short description of your project',
+        name: 'description'
+    },
+    {
+        type: 'input',
+        message: 'What kind of license should your project have?',
+        name: 'licence'
+    },
+    {
+        type: 'input',
+        message: 'What command should be run to install dependencies?',
+        name: 'depend'
+    },
+    {
+        type: 'input',
+        message: 'What command should be used to run tests?',
+        name: 'test'
+    },
+    {
+        type: 'input',
+        message: 'What does the user need to know about using the repo?',
+        name: 'useUser'
+    },
+    {
+        type: 'input',
+        message: 'What does the user need to know about contributing to the repo',
+        name: 'userContr'
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
